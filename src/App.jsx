484071270b3606b0507a9f823d8ad03710246804
Dipayan Banik <dipayan158@gmail.com>
2022-06-26 13:52:30 +0530

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1>Welcome to React Vite Micro App!</h1>
+      <p>Hard to get more minimal than this React app.</p>
+      <b>Kuttar Baccha</b>
+    </>
+  );
+};
+
+export default App;
